70b119d6d366a7fa2da6bc1bdd634e54cf8ee367
emilhals <emilhals.i@gmail.com>
2024-03-02 18:38:14 +0100

    Added Header from TailwindUI

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+import Header from './components/Header.vue'
 </script>
 
 <template>
-  <div>
-    <h3 class="font-bold text-3xl">Kulde.app</h3>
+  <div class="layout">
+    <header>
+      <Header />
+    </header>
+
+    <RouterView />
   </div>
 </template>
---
+++ src/views/TroubleshooterView.vue
@@ -0,0 +1,3 @@
+<template>
+  <h3>Feilsøker</h3>
+</template>
